f08b1ed55c6da3e8f0418d709e36129a5498dcb9
unknown <info@mediagramstudios.com>
2021-02-22 08:55:57 +0800

    initial-commit

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,30 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, :brightness, 10)
+    {:ok, socket}
+    # {:ok, assign(socket, :brightness, 10)}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Front Porch Light </h1>
+    <div id="light">
+      <div class="meter">
+        <span style="width: <%= @brightness %>%">
+          <%= @brightness %>%
+        </span>
+      </div>
+
+      <button>
+        <img src="images/light-off.svg">
+      </button>
+
+      <button>
+        <img src="images/light-on.svg">
+      </button>
+    </div>
+    """
+  end
+end
