52758934fc56bcad64f7a296f21ba78a63bffa32
unknown <info@mediagramstudios.com>
2021-03-22 09:20:06 +0800

    wire up form events

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -1,9 +1,10 @@
 defmodule LiveViewStudioWeb.LicenseLive do
   use LiveViewStudioWeb, :live_view
   alias LiveViewStudio.Licenses
+  import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 2, amout: Licenses.calculate(2))
+    socket = assign(socket, seats: 2, amount: Licenses.calculate(2))
     {:ok, socket}
   end
 
@@ -16,17 +17,25 @@ defmodule LiveViewStudioWeb.LicenseLive do
           <img src="images/license.svg" />
           <span>
             Your license is currently for
-            <strong>2</strong> seats.
+            <strong><%= @seats %></strong> seats.
           </span>
         </div>
 
-        <form>
-          <input type="range" min="1" max="10" name="seats" value="2" />
+        <form phx-change="update">
+          <input type="range" min="1" max="10" name="seats" value="<%= @seats %>" />
         </form>
 
-        <div class="amount">$40</div>
+        <div class="amount"><%= number_to_currency(@amount) %></div>
       </div>
     </div>
     """
+  end
+
+  def handle_event("update", %{"seats" => seats}, socket) do
+  seats = String.to_integer(seats)
+    socket = assign(socket, 
+        seats: seats,
+        amount: Licenses.calculate(seats))
+    {:noreply, socket}
   end
 end
